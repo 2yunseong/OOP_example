dff743413816248d586b3d4c846f08f76a1b3c10
yunseong76039287 <56749516+yunseong76039287@users.noreply.github.com>
2020-01-19 20:09:07 +0900

    Update global_operator_example.cpp
    Added just one operator about compound assignment operators, and binary arithmetic operators.(+=, +)
    I'll add all later

--- operator_overloading/global_operator_example.cpp
+++ operator_overloading/global_operator_example.cpp
@@ -14,14 +14,14 @@ public:
         cout << "Data:" << x << endl;
         cout << "Data:" << y << endl;
     }
-    Data& operator=(const Data& ref)   
+    Data &operator=(const Data &ref) //assignment operator(only)
     {
         //  must overload operator= to member function.
         //  and it allow right associative.(primitive type)
         //  and operator chaining too.
 
-        if(this == &ref)    //if Same object?
-            return *this;   //just return itself(this).
+        if (this == &ref) //if Same object?
+            return *this; //just return itself(this).
 
         //must check self-assignment.
         //if not check self-assignment, it make a series of problem.
@@ -34,13 +34,29 @@ public:
         return *this;
 
     } //Q. what is non_static_member_function?
-};
 
+    Data &operator+=(const Data &ref) //compound assignment operators
+    {
+        //return type Data& can write that
+        //for example, (class+=3)+=5
+        x += ref.x;
+        x += ref.y;
+        return *this;
+    } //hw.make -=, *=.
+
+    const Data operator+(const Data &ref) const //binary arithmetic operators
+    {
+        //use compound assignment operator!
+        Data copy = *this; //same as Data copy(*this);
+        copy += ref;
+        return result;
+    }
+};
 
 int main()
 {
-    Data dt1(2,2) ;
-    Data dt2(3,3) ;
+    Data dt1(2, 2);
+    Data dt2(3, 3);
 
-    dt1= dt2; //same as dt1.operator=(dt2);
+    dt1 = dt2; //same as dt1.operator=(dt2);
 }
