d435941c51e1d7dd5593ece4da0357cc7f938242
yunseong76039287 <56749516+yunseong76039287@users.noreply.github.com>
2020-01-19 23:02:26 +0900

    Standard_Template_Library
    1.vector

---
+++ Standard Template Library/container/vector.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> vData;           //declare vector.: vector<type> name
+    vector<int> vData2(5);      // declare & initialize. value = 0, array length = 5 . [0][0][0][0][0]
+    vector<int> vData3(5,2);    // vector<type> name(length, initial_value) [2][2][2][2][2]
+    
+}
