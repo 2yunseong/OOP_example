a3fa7d8a15155a38ac5ed2b3b7ed8dab6293cbc7
yunseong76039287 <56749516+yunseong76039287@users.noreply.github.com>
2020-01-19 23:05:50 +0900

    Update global_operator_example.cpp
    1.More comment(Regardless of readability)
    2.Add prefix increment/ decrement operator.

--- operator_overloading/global_operator_example.cpp
+++ operator_overloading/global_operator_example.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
-
+/*
+1.compound assignment operator +=, -=, *= ...
+2.binary arithmatic operator +,-,* ... and so on
+*/
 class Data
 {
 private:
@@ -14,20 +17,26 @@ public:
         cout << "Data:" << x << endl;
         cout << "Data:" << y << endl;
     }
+
+    /**
+         *   must overload operator= to member function.
+         * because This operator makes sense if you target with the object.
+         *  and it allow right associative.(primitive type)
+         *  and operator chaining too.
+         */
+
+    /**
+        * must check self-assignment.
+        * if not check self-assignment, it make a series of problem.
+        * in main function, declared class 'Data dt'
+        *and then, 'dt = dt'(this '' code maybe make a problem.)
+        *that's why we check self- assignment.
+        */
+
     Data &operator=(const Data &ref) //assignment operator(only)
     {
-        //  must overload operator= to member function.
-        //  and it allow right associative.(primitive type)
-        //  and operator chaining too.
-
         if (this == &ref) //if Same object?
             return *this; //just return itself(this).
-
-        //must check self-assignment.
-        //if not check self-assignment, it make a series of problem.
-        // in main function, declared class 'Data dt'
-        //and then, 'dt = dt'(this '' code maybe make a problem.)
-        //that's why we check self- assignment.
 
         x = ref.x;
         y = ref.y;
@@ -37,19 +46,40 @@ public:
 
     Data &operator+=(const Data &ref) //compound assignment operators
     {
-        //return type Data& can write that
-        //for example, (class+=3)+=5
+        /**
+        * return type Data& can write that
+        * for example, (class+=3)+=5
+        */
+
         x += ref.x;
         x += ref.y;
         return *this;
+
     } //hw.make -=, *=.
 
-    const Data operator+(const Data &ref) const //binary arithmetic operators
+    const Data operator+(const Data &other) const //binary arithmetic operators
     {
+        //1. return const type: return const Data value.
         //use compound assignment operator!
-        Data copy = *this; //same as Data copy(*this);
-        copy += ref;
-        return result;
+
+        Data copy = *this; //make copy object :: this code same as Data copy(*this);
+        copy += other;     //now, plus other object on copy. use previous function,(+=) the code are Shorten.
+        return copy;
+    }
+    /* p r e f i x o p e r a t o r */
+
+    Data &operator++() //prefix increment operator.(in book.)
+    {
+        x += 1;
+        y += 1;
+        return *this;
+    }
+
+    Data &operator--() //prefix decrement operator.(in myhead.) (in the book, this function declared on global scope.)
+    {
+        x -= 1;
+        y -= 1;
+        return *this;
     }
 };
 
